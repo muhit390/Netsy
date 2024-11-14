979c0f8223fb56140d41989d4b2812d2a9909fa8
karlhdenby <karlhdenby@gmail.com>
2024-11-13 18:53:22 -0800

    sharing

--- app/api/images.py
+++ app/api/images.py
@@ -1,18 +1,50 @@
-from flask import Blueprint, jsonify, request
-from app.models import ProductImage, db
+from flask import Blueprint, jsonify, request, render_template, redirect
+from ..models import ProductImage
+from app.models import db
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField
-from .aws_helpers import ALLOWED_EXTENSIONS
+from aws_helpers import ALLOWED_EXTENSIONS, upload_file_to_s3, get_unique_filename
+from app.models import db, Image
+from flask_login import current_user, login_required
+
+class ImageForm(FlaskForm):
+    image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    submit = SubmitField("Create Post")
+
 
 images_bp = Blueprint('images', __name__)
 
 @images_bp.route('/products/<int:product_id>/images', methods=['POST'])
+@login_required
 def add_product_image(product_id):
-    data = request.get_json()
+    data = ImageForm()
+    data["csrf_token"].data = request.cookies["csrf_token"]
+
+    if data.validate_on_submit():
+          
+        image = data.data["image"]
+        image.filename = get_unique_filename(image.filename)
+        upload = upload_file_to_s3(image)
+        print(upload)
+
+        if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when you tried to upload
+        # so you send back that error message (and you printed it above)D
+            return render_template("post_form.html", form=data, errors=[upload])
+        
+        url = upload["url"]
+        db.session.add(new_image)
+        db.session.commit()
+        return redirect("/posts/all")
 
     if not data.get('name'):
         return jsonify({"message": "Image name is required"}), 400
+    
+    if data.errors:
+        print(data.errors)
+        return render_template("post_form.html", form=data, errors=data.errors)
 
     new_image = ProductImage(
         product_id=product_id,
@@ -20,13 +52,15 @@ def add_product_image(product_id):
         preview_image=data.get('preview_image', False)
     )
     
-    try:
-        db.session.add(new_image)
-        db.session.commit()
-        return jsonify({"message": "Image added successfully"}), 201
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({"message": "Error adding image", "error": str(e)}), 500
+    return jsonify([
+        {
+            "id": new_image.id,
+            "productId": new_image.productId,
+            "name": new_image.name,
+            "previewImage": new_image.previewImage
+        }
+    ])
+    
 
 @images_bp.route('/products/<int:product_id>/images', methods=['GET'])
 def get_product_images(product_id):
